e9f2a9f9b8f1c5404eec363cb10f776a7b3acea9
acstash <acdevhub@gmail.com>
2021-08-02 21:42:54 +0200

    How Many ?!

--- howmany.py
+++ howmany.py
@@ -9,7 +9,7 @@ digits = list(string.digits)
 # Ask for the sentence
 print('Hi! Enter a sentence and I will tell you exactly how many letters and numbers it has :)')
 sentence = str(input())
-# With splice we add each letter/digit/symbol/space of the sentence into a list
+# With splice we add each letter/digit/symbol/space of the sentence into a list.
 sentence_splice[:] = sentence
 # Iterating the spliced sentence, with a few counters and conditionals we get the number of each digit,
 # word and symbol. We also ignored blank spaces.
